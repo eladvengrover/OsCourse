ae3eb0fa7985256dbb96dcfc736fa1813655fdd0
eladvengrover <eladv@mail.tau.ac.il>
2024-02-02 19:26:47 +0200

    changed architecture

--- Documents/osCourse/myshell.c
+++ Documents/osCourse/myshell.c
@@ -16,10 +16,11 @@ typedef enum {
     REG_BG = 2,
     SING_PIPE = 3,
     INPUT = 4,
-    OUTPUT = 5
+    OUTPUT = 5,
+    NOT_VALID = -1
 } OperationType;
 
-int child_handler(int count, char** arglist, OperationType op);
+int operation_handler(int count, char** arglist, OperationType op);
 int handle_reg_op(int count, char** arglist);
 int handle_reg_bg_op(int count, char** arglist);
 int handle_pipe_op(int count, char** arglist);
@@ -38,72 +39,88 @@ int prepare(void) {
 int process_arglist(int count, char** arglist) {
     OperationType op = get_operation_type(count, arglist);
     printf("op: %d\n", op);
+
+    return operation_handler(count, arglist, op);
+}
+
+int operation_handler(int count, char** arglist, OperationType op) {
+    switch (op) {
+        case REG:
+            return handle_reg_op(count, arglist);
+        case REG_BG:
+            return handle_reg_bg_op(count, arglist);
+        case SING_PIPE:
+            return handle_pipe_op(count, arglist);
+        case INPUT:
+            return handle_input_op(count, arglist);
+        case OUTPUT:
+            return handle_output_op(count, arglist);
+        default:
+            return 0;
+    }
+}
+
+int handle_reg_op(int count, char** arglist) {
+    int pid = fork();
+    if (pid == -1) {
+        return 0;
+    }
+    if (pid == 0) {
+        // Child proccess
+        if (execvp(arglist[0], arglist) == -1) {
+        perror("execvp failed!");
+        exit(1);
+    }
+    } else {
+        // Parent proccess
+        int status;
+        if (waitpid(pid, &status, 0) == -1) {
+            return 0;
+        }
+        return 1;
+    }
+
+
+    
+    return 1;
+}
+
+int handle_reg_bg_op(int count, char** arglist) {
+    int pid = fork();
+    if (pid == -1) {
+        return 0;
+    }
+    if (pid == 0) {
+        // Child proccess
+        free((char*) arglist[count]);
+        arglist[count - 1] = NULL;
+        
+        if (execvp(arglist[0], arglist) == -1) {
+            perror("execvp failed!");
+            exit(1);
+        }
+    }
+    return 1;
+}
+
+int handle_pipe_op(int count, char** arglist) {
+    int pfds[2];
+    if (pipe(pfds) == -1) {
+        return -1;
+    }
     int pid = fork();
     if (pid == -1) {
         return -1;
     }
     if (pid == 0) {
         // Child proccess
-        return child_handler(count, arglist, op);
+        close(pfds[0]);
     } else {
         // Parent proccess
-        int status, child_pid;
-        if (op != REG_BG) {
-            child_pid = waitpid(pid, &status, 0);
-            if (child_pid == -1) {
-                return -1;
-            }
-         }
-
-        return 1;
-    }
     
-}
-
-int child_handler(int count, char** arglist, OperationType op) {
-    switch (op) {
-        case REG:
-            return handle_reg_op(count, arglist);
-            break;
-        case REG_BG:
-            return handle_reg_bg_op(count, arglist);
-            break;
-        case SING_PIPE:
-            break;
-        case INPUT:
-            break;
-        case OUTPUT:
-            break;
-        default:
-            break;
     }
-    return 0;
-}
 
-int handle_reg_op(int count, char** arglist) {
-    printf("handling reg op\n");
-    if (execvp(arglist[0], arglist) == -1) {
-        perror("execvp failed!");
-        exit(1);
-    }
     return 1;
-}
-
-int handle_reg_bg_op(int count, char** arglist) {
-    printf("handling reg bg op\n");
-
-    free((char*) arglist[count]);
-    arglist[count - 1] = NULL;
-
-    if (execvp(arglist[0], arglist) == -1) {
-        perror("execvp failed!");
-        exit(1);
-    }
-    return 1;
-}
-
-int handle_pipe_op(int count, char** arglist) {
-    return 0;
 }
 
 int handle_input_op(int count, char** arglist) {
@@ -129,6 +146,7 @@ OperationType get_operation_type(int count, char **arglist) {
             return SING_PIPE;
         }
     }
+    // TODO - add check for command validation
     return REG;
 }
 
