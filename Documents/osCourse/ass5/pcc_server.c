339cc36deb98ad17694836b76f9ab8cd00304ff8
eladvengrover <eladv@mail.tau.ac.il>
2024-03-08 17:00:28 +0200

    add server

---
+++ Documents/osCourse/ass5/pcc_server.c
@@ -0,0 +1,86 @@
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <sys/stat.h>
+
+
+int main(int argc, char *argv[]) {
+    uint16_t pcc_total[95] = { 0 };
+    uint16_t server_port, N_net, N;
+    int file_fd, server_socket_fd, client_socket_fd;
+    struct sockaddr_in server_address, client_address;
+    socklen_t client_len;
+    char *buffer;
+    ssize_t bytes_read, total_read;
+
+    if (argc != 2)  {
+        perror("Invalid number of args");
+        exit(1);
+    }
+
+    server_socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_socket_fd < 0) {
+        perror("Failed opening a socket");
+        exit(1);
+    }
+
+    server_address.sin_family = AF_INET;
+    server_address.sin_addr.s_addr = INADDR_ANY;
+    server_address.sin_port = htons(argv[1]);
+
+    if (bind(server_socket_fd, (struct sockaddr *) &server_address, sizeof(server_address) < 0)) {
+        perror("Failed binding the server");
+        exit(1);
+    }
+
+    listen(server_socket_fd, 10);
+
+    while (1) {
+        client_len = sizeof(client_address);
+        client_socket_fd = accept(server_socket_fd, (struct sockaddr *)&client_address, &client_len);
+        if (client_socket_fd < 0) {
+            perror("Failed accepting a client");
+            continue;
+        }
+        // Read the number N
+        bytes_read = read(client_socket_fd, &N_net, sizeof(N_net));
+        if (bytes_read < 0) {
+            perror("Failed reading N from the client\n");
+            continue;
+        }
+        N = ntohs(N_net);
+        buffer = malloc(N);
+        if (buffer == NULL) {
+            perror("Failed allocating memory\n");
+            continue;
+        }
+        memset(buffer, 0, N);
+        total_read = 0;
+        while (total_read < N) {
+            bytes_read = read(client_socket_fd, buffer + total_read, N - total_read);
+            if (bytes_read < 0) {
+                perror("Failed reading N from the client\n");
+                break;
+            }
+            total_read += bytes_read;
+        }
+        if (total_read == N) {
+            printf("Counting printable chars");
+        }
+        free(buffer);
+        close(client_socket_fd);
+    }
+    
+
+
+
+}
